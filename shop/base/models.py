ff6f4c0528b46f4fe5878bf203cb03a60019477e
serega-s <comp.it.05@inbox.ru>
2021-09-24 18:25:23 +0300

    added sorting

--- shop/base/models.py
+++ shop/base/models.py
@@ -16,9 +16,13 @@ class Customer(models.Model):
     email = models.CharField(max_length=255)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.name)
+    
+    class Meta:
+        ordering = ['-created_at']
 
 
 User.customer = property(
